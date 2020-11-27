2b77159d172603b30213b577c171f5a25dc81d81
Angana1 <75121801+Angana1@users.noreply.github.com>
2020-11-27 16:34:10 +0530

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,28 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main()
+{
+    string s1,s2;
+    int i;
+    cin>>s1;
+    cin>>s2;
+    transform(s1.begin(),s1.end(),s1.begin(),::toupper);
+    transform(s2.begin(),s2.end(),s2.begin(),::toupper);
+
+
+
+
+
+    if(s1.compare(s2)<0)
+        cout<<-1;
+    else if(s1.compare(s2)>0)
+    {
+        cout<<1;
+    }
+    else
+        cout<<0;
+
+    return 0;
+}
